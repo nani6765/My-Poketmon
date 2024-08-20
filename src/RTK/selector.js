e2030ba312cee09a766babb6c06dac3c536658d3
dnwjd0218 <dnwjd1325@naver.com>
2024-08-21 05:13:49 +0900

    포켓몬 도감 만들기(1/2)

---
+++ src/RTK/selector.js
@@ -0,0 +1,8 @@
+import { createSelector } from "@reduxjs/toolkit";
+
+export const selectPokemonById = (pokemonId) =>
+    createSelector(
+    (state) => state.pokemon.data, 
+    (pokemon) => {if (!pokemon) return undefined
+        return pokemon.find((el) => el.id === pokemonId)
+    });
---
+++ src/pages/Main.jsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux"
+import { Card } from "../component/Card";
+
+export default function Main() {
+    const pokemonData = useSelector(state => state.pokemon)
+
+    // 데이터가 아직 로드되지 않았을 경우 로딩 상태 보여줌
+    if (!pokemonData || !pokemonData.data) {
+        return <div>Loading...</div>;
+    }
+
+    return(
+    <>
+        {pokemonData.data.map(el => <Card key={el.id} pokemon={el}/>)}
+    </>
+    )
+}
